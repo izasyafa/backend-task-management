5add582b27724aee805d632b03086df4bae4b903
izasyafa <arkaassyafa00@gmail.com>
2025-09-12 10:09:16 +0700

    setup project

---
+++ app/Models/Task.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'due_date',
+        'time_tracked',
+        'status',
+        'priority'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2025_09_12_025103_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->dateTime('due_date');
+            $table->integer('time_tracked')->default(0);
+            $table->enum('status', ['pending', 'open', 'in_progress', 'completed'])->default('pending');
+            $table->enum('priority', ['low', 'medium', 'high'])->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
